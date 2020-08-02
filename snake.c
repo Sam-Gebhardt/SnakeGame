5ef8dc4ae7360dbd224ecc3bccb7b94a61f472fc
Sam Gebhardt <samgeb35@gmail.com>
2020-08-01 22:39:38 -0700

    Started implementing the snake body with a double ll

--- snake.c
+++ snake.c
@@ -10,22 +10,14 @@ Basic terminal snake game
 #include <ncurses.h>
 #include <time.h>
 
-// void border_make(struct winsize size) {
-	
-// 	int height = size.ws_row - 2;
-// 	for (int i = 0; i < height; i++) {
-// 		if (i == 0 || i == height - 1) {
-// 			// printf("%*s%s", size.ws_col, "*", ""); 
-// 			// printf("%s%019s\n", "buffer", "*");
-// 			for (int j = 0; j < size.ws_col; j++) {
-// 				printf("*");
-// 			}
-// 			printf("\n");
-// 			continue;
-// 		}
-// 		printf("*%*s", size.ws_col - 1, "*");
-// 	}
-// }
+typedef struct node {
+	struct node *next;
+	struct node *prev;
+	struct node *head;
+	struct node *tail;
+	int x_cord;
+	int y_cord;
+} Node;
 
 char *head_of_the_snake(int direction_x, int direction_y) {
 	char *head;
@@ -57,47 +49,96 @@ void random_int(int out[2], int max_x, int max_y) {
 	out[1] = (rand() % max_y);
 }
 
+// void add_node(struct *Node list, int x_cord, int y_cord) {
+
+// }
+
+
+const Node *create_snake(void) {
+
+	const Node *snake_body = (Node*)malloc(sizeof(Node*));
+	return snake_body;
+}
+
 int main() {
+
 	int x, y, max_x, max_y;
-	int direction_x = 0, direction_y = 1;
-	int next_x, next_y;
+	int direction_x = 1, direction_y = 0;
+	int uneaten_apple = 0;
 
 
 	initscr();
 	noecho();  // no keyboard input
 	curs_set(FALSE);
+	keypad(stdscr, TRUE);
+
 	getmaxyx(stdscr, max_y, max_x);
 	mvprintw(max_y / 2 - 1, max_x  / 2 - 7, "Classic Snake");
 	mvprintw(max_y / 2, max_x / 2, ">");
+
 	x = max_x / 2;
 	y = max_y / 2;
-	refresh();
 
+	snake = create_snake();
+	refresh();
 	getchar();
+	nodelay(stdscr, true);
 
 	while(1) {
-		int apple[2];
-		// int leading_char;
 
-		clear();
+		int apple[2];
+		int move = getch();
+
+		switch(move) {
+			case KEY_UP:
+				direction_x = 0; direction_y = -1;
+				break;
+			case KEY_DOWN:
+				direction_x = 0; direction_y = 1;
+				break;
+			case KEY_RIGHT:
+				direction_x = 1; direction_y = 0;
+				break;
+			case KEY_LEFT:
+				direction_x = -1; direction_y = 0;
+				break;
+			case 27:  // esc
+				clear();
+				endwin();
+				return 0;
+		}
+	
+
+		refresh();
 		getmaxyx(stdscr, max_y, max_x);
 		char *head = head_of_the_snake(direction_x, direction_y);
 		mvprintw(y, x, head);
 
-		// refresh();
-		next_x = x + direction_x;
-		next_y = y + direction_y;
-		if (next_x > max_x || next_x < 0) {
-			direction_x *= -1;
-		} else if (next_y > max_y || next_y < 0) {
-			direction_y *= -1;
+		if ((x > max_x || x < 0) || (y > max_y || y < 0)) {
+
+			clear();
+			mvprintw(max_y / 2, max_x / 2 - 5, "Game Over!");
+			refresh();
+			getchar();
+			endwin();
+			return 0;
 		}
+		
 		x += direction_x;
 		y += direction_y;
-		usleep(150000);
-		random_int(apple, max_x, max_y);
-		mvprintw(apple[1], apple[0], "%d,%d", apple[0], apple[1]);
-		refresh();
+
+		if (direction_y) {
+			usleep(150000);
+		} else {
+			usleep(37500);
+		}
+		if (! uneaten_apple) {
+			uneaten_apple = 1;
+			random_int(apple, max_x, max_y);
+			mvprintw(apple[1], apple[0], "@");
+			refresh();
+
+		}
 
 	}
 	endwin();
@@ -120,4 +161,9 @@ b. Display "Game over" and score
 c. Increase score by 1 and snake body by 1
 
 Hold snake body in linked list?
+	doubly liked list-
+	allows movement in constent time(Move last * to the front)
+	add len by adding * after head of the snake
+	checking if the snake ate itself by looping though ll
+		not ideal with O(n), but the only way I think
 */
